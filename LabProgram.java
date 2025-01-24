b2b739a81a93b6499ed45fa73f483abbba3d8c13
Preston T <PrestonThronson@gmail.com>
2025-01-24 15:34:36 -0800

    deleted half

--- LabProgram.java
+++ LabProgram.java
@@ -31,32 +31,7 @@ public class LabProgram{
 		System.out.println("          ../, `.            `  |    .\\`.           \\ \\_");
 		System.out.println("         ,' ,..  .           _.,'    ||\\l            )  '\".");
 		System.out.println("        , ,'   \\           ,'.-.`-._,'  |           .  _._`.");
-		System.out.println("      ,' /      \\ \\        `' ' `--/   | \\          / /   ..\\");
-		System.out.println("    .'  /        \\ .         |\\__ - _ ,'` `        / /     `.`.");
-		System.out.println("    |  '          ..         `-...-\"  |  `-'      / /        . `.");
-		System.out.println("    | /           |L__           |    |          / /          `. `.");
-		System.out.println("   , /            .   .          |    |         / /             ` `");
-		System.out.println("  / /          ,. ,`._ `-_       |    |  _   ,-' /               ` \\");
-		System.out.println(" / .           \\\"`_/. `-_ \\_,.  ,'    +-' `-'  _,        ..,-.    \\`.");
-		System.out.println(".  '         .-f    ,'   `    '.       \\__.---'     _   .'   '     \\ \\");
-		System.out.println("' /          `.'    l     .' /          \\..      ,_|/   `.  ,'`     L`");
-		System.out.println("|'      _.-\"\"` `.    \\ _,'  `            \\ `.___`.'\"`-.  , |   |    | \\");
-		System.out.println("||    ,'      `. `.   '       _,...._        `  |    `/ '  |   '     .|");
-		System.out.println("||  ,'          `. ;.,.---' ,'       `.   `.. `-'  .-' /_ .'    ;_   ||");
-		System.out.println("|| '              V      / /           `   | `   ,'   ,' '.    !  `. ||");
-		System.out.println("||/            _,-------7 '              . |  `-'    l         /    `||");
-		System.out.println(". |          ,' .-   ,' ||               | .-.        `.      .'     ||");
-		System.out.println(" `'        ,'    `\".'    |               |    `.        '. -.'       `'");
-		System.out.println("          /      ,'      |               |,'    \\-.._,.'/'");
-		System.out.println("          .     /        .               .       \\    .''");
-		System.out.println("        .`.    |         `.             /         :_,'.'");
-		System.out.println("          \\ `...\\   _     ,'-.        .'         /_.-'");
-		System.out.println("           `-.__ `,  `'   .  _.>----''.  _  __  /");
-		System.out.println("                .'        /\"'          |  \"'   '_");
-		System.out.println("               /_|.-'\\ ,\".             '.'`__'-( \\");
-		System.out.println("                 / ,\"'\"\\,'               `/  `-.|\"");
-		System.out.println("                           I Choose You!");
-		System.out.println("                           Gotta Catch em All!");
+
 
 	}
 }
